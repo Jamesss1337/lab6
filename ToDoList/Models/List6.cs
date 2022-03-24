72bf87fa3175bbbb3442e19cad3f287ab1af8ca7
Yakov Pivchik <ya_piv@mail.ru>
2022-03-24 23:19:19 +0700

    ToDoList

---
+++ ToDoList/Models/List6.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class List6
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public List6(string title, string description)
+        {
+            this.Title = title;
+            this.Description = description;
+
+        }
+        
+    }
+}
